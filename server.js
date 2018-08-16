7aa37882853c057efb048d96683c00860abf06cb
Spik3x <pelaezas007@gmail.com>
2018-08-15 21:04:22 -0500

    Crear paginas de empresa

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 var express = require("express");
 var app=express();
+var path= require("path");
+
+app.get("/empresa",function(req,res){
+    res.sendFile(
+        path.join(
+            __dirname, "/src/views/empresa.html"
+        )
+    );
+});
+
+app.get("/verempresa",function(req,res){
+    res.sendFile(
+        path.join(
+            __dirname, "/src/views/verempresa.html"
+        )
+    );
+});
+
 app.get("/",function(req,res){
-    res.send("Hola");
+    res.sendFile(
+        path.join(
+            __dirname, "/src/views/index.html"
+        )
+    );
 });
 
 app.listen(3000, function(){
